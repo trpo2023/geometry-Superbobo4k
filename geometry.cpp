1da1a9bf10f553f1e6731d54355463d6755922bc
Andreas Vaibert <services.weibert@gmail.com>
2023-03-02 17:01:44 +0700

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -1,232 +1,247 @@
 #include <algorithm>
-#include <cctype>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cctype>
 #define _USE_MATH_DEFINES
 #include <math.h>
-
 using namespace std;
 
-ofstream outFile;
-
 string line;
+int circleCounter;
+double circleArray[100][100];
 
-int errorMessage(int pointer, string str)
-{
-    ofstream outFile;
-    outFile.open("geometry-output");
-    outFile << line << endl;
-    int x = 0;
-    for (x = x; x < pointer; x++) {
-        outFile << ' ';
-    }
-    outFile << '^' << endl << str;
-    return 0;
+int errorMessage(int position, string message) {
+	cout << circleCounter + 1 << ". " << line << endl;
+	int x = 0;
+	for (x = x; x < position; x++) {
+		cout << ' ';
+	}
+	cout << "   " << '^' << endl << message << endl;
+	return 0;
 }
 
-int validNumber(string str, int pointer)
-{
-    outFile.open("geometry-output");
-
-    size_t k = 0;
-    size_t l = str.length() - 1;
-
-    string expectedNumber = "expected number";
-
-    // if string is of length 1 and the only
-    // character is not a digit
-    if (k == l && !(str[k] >= '0' && str[k] <= '9')) {
-        errorMessage(pointer + k, expectedNumber);
-        return 0;
-    }
-    // If the 1st char is not '+', '-', '.' or digit
-    if (str[k] != '.' && str[k] != '+' && str[k] != '-'
-        && !(str[k] >= '0' && str[k] <= '9')) {
-        errorMessage(pointer + k, expectedNumber);
-        return 0;
-    }
-
-    // To check if a '.' is found in given
-    // string. We use this flag to make sure that
-    // either of them appear only once.
-    bool flagDot = false;
-
-    for (k = k; k <= l; k++) {
-        // If any of the char does not belong to
-        // {digit, .}
-        if (str[k] != '.' && !(str[k] >= '0' && str[k] <= '9')) {
-            errorMessage(pointer + k, expectedNumber);
-            return 0;
-        }
-
-        // checks if the char '.' has already
-        // occurred before '.' If yes, return 0.
-        if (str[k] == '.') {
-            if (flagDot == true) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-
-            // If '.' is the last character.
-            if (k + 1 > str.length()) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-
-            // if '.' is not followed by a digit.
-            if (!(str[k + 1] >= '0' && str[k + 1] <= '9')) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-        }
-
-        else if (str[k] == '.') {
-            // set flagDot = 1 when . is encountered.
-            flagDot = true;
-
-            // if there is no digit before '.'.
-            if (!(str[k - 1] >= '0' && str[k - 1] <= '9')) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-
-            // If '.' is the last Character
-            if (k + 1 > str.length()) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-
-            // if . is not followed either by a digit
-            if (!(str[k + 1] >= '0' && str[k] <= '9')) {
-                errorMessage(pointer + k, expectedNumber);
-                return 0;
-            }
-        }
-    }
-
-    /* If the string skips all above cases, then
-    it is numeric*/
-    return 1;
+int print() {
+	for (int i = 0; i < circleCounter; i++) {
+		cout << i + 1 << ". " << "circle(" << circleArray[i][0] << ' ' << circleArray[i][1] << ", " << circleArray[i][2] << ')' << endl
+			<< '	' << "perimeter = " << circleArray[i][3] << endl
+			<< '	' << "area = " << circleArray[i][4] << endl;
+		if (circleArray[i][5] > 0) {
+			cout << '	' << "intersects: " << endl;
+			for (int j = 0; j < circleArray[i][5]; j++) {
+				cout << "		" << circleArray[i][6 + j] << ". circle" << endl;
+			}
+		}
+	}
+	return 0;
 }
 
-const int maxLength = 20;
-int p;
-int i;
-size_t g;
-
-int nToArray(char seperator, double array[maxLength])
-{
-    ofstream outFile;
-    bool flag = false;
-    for (g = g; g < line.length(); g++) {
-        if (line[g] == seperator) {
-            flag = true;
-            break;
-        }
-    }
-    if (flag == true) {
-        string number = line.substr(p, g - p);
-        if (validNumber(number, p)) {
-            // convert string to double
-            double num_double = stod(number);
-            array[i] = num_double;
-            i++;
-            g++;
-            p = g;
-            return 1;
-        } else {
-            return 0;
-        }
-    } else {
-        outFile.open("geometry-output");
-        outFile << line << endl << "expected seperator between numeric values";
-        return 0;
-    }
+int circleCalculation() {
+	for (int g = 0; g < circleCounter; g++) {
+		circleArray[g][3] = 2 * M_PI * circleArray[g][2];
+		circleArray[g][4] = M_PI * pow(circleArray[g][2], 2);
+		circleArray[g][5] = 0;
+	}
+	for (int g = 0; g <= circleCounter; g++) {
+		for (int h = g + 1; h <= circleCounter; h++) {
+			if (pow((circleArray[g][2] - circleArray[h][2]), 2) <= pow((circleArray[g][0] - circleArray[h][0]), 2) + pow((circleArray[g][1] - circleArray[h][1]), 2)
+				&& pow((circleArray[g][0] - circleArray[h][0]), 2) + pow((circleArray[g][1] - circleArray[h][1]), 2) <= pow((circleArray[g][2] + circleArray[h][2]), 2)) {
+				int temp = circleArray[g][5];
+				circleArray[g][6 + temp] = h+1;
+				circleArray[g][5] += 1;
+				temp = circleArray[h][5];
+				circleArray[h][6 + temp] = g+1;
+				circleArray[h][5] += 1;
+			}
+		}
+	}
+	return 0;
 }
 
-void circle()
-{
-    if (line.rfind('(', 6) == 6) {
-        double circleArray[maxLength];
-        g = 0;
-        p = 7;
-        i = 0;
-        if (nToArray(' ', circleArray)) {
-            if (nToArray(',', circleArray)) {
-                g++;
-                p++;
-                if (nToArray(')', circleArray)) {
-                    if (g == line.length()) {
-                        double circlePerimeter = 2 * M_PI * circleArray[2];
-                        double circleArea = M_PI * pow(circleArray[2], 2);
-                        ofstream outFile;
-                        outFile.open("geometry-output");
-                        outFile << "1. " << line << endl
-                                << "    perimeter = " << circlePerimeter << endl
-                                << "    area = " << circleArea;
-                    } else {
-                        errorMessage(g, "expected end of line");
-                    }
-                }
-            }
-        }
-    } else {
-        outFile.open("geometry-output");
-        outFile << line << endl
-                << "      ^" << endl
-                << "expected '(', at position 6";
-    }
+int checkNumber(size_t &p, int &j) {
+
+	// If the 1st char is not '+', '-', or digit
+	if (line[p] != '+' && line[p] != '-'
+		&& !(line[p] >= '0' && line[p] <= '9')) {
+		errorMessage(p, "expected Number");
+		return 0;
+	}
+
+	int start = p;
+
+	p += 1;
+
+	bool flagDot = false;
+
+	for (p = p; p < line.length(); p++) {
+
+		if (line[p] == ' ') {
+			int end = p;
+			string number = line.substr(start, end - start);
+			double num_double = stod(number);
+			circleArray[circleCounter][j] = num_double;
+			p++;
+			return 1;
+		}
+
+		if (line[p] == ',' && j == 1) {
+			int end = p;
+			string number = line.substr(start, end - start);
+			double num_double = stod(number);
+			circleArray[circleCounter][j] = num_double;
+			return 1;
+		}
+
+		if (line[p] == ')' && j == 2) {
+			if (line.length() > p + 1) {
+				errorMessage(p + 1, "expected end of line");
+				return 0;
+			}
+			int end = p;
+			string number = line.substr(start, end - start);
+			double num_double = stod(number);
+			circleArray[circleCounter][j] = num_double;
+			return 1;
+		}
+
+		// If any of the char after the first does not belong to
+		// {digit, .}
+		if (line[p] != '.'
+			&& !(line[p] >= '0' && line[p] <= '9')) {
+			errorMessage(p, "expected Number");
+			return 0;
+		}
+		// checks if the char '.' has already occurred before
+		if (line[p] == '.') {
+			if (flagDot == true) {
+				errorMessage(p, "expected Number");
+				return 0;
+			}
+			// If '.' is the last character.
+			if (p + 1 > line.length()) {
+				errorMessage(p, "expected Number");
+				return 0;
+			}
+			// if '.' is not followed by a digit.
+			if (!(line[p + 1] >= '0' && line[p + 1] <= '9')) {
+				errorMessage(p + 1, "expected Number");
+				return 0;
+			}
+			// if there is no digit before '.'.
+			if (!(line[p - 1] >= '0' && line[p - 1] <= '9')) {
+				errorMessage(p, "expected Number");
+				return 0;
+			}
+			// set flagDot = 1 when '.' is encountered.
+			flagDot = true;
+		}
+	}
+	errorMessage(p, "expected input");
+	return 0;
 }
 
-void triangle()
-{
+int parsing(size_t &p) {
+	for (int j = 0; j < 3; j++) {
+		if (line.length() == p) {
+			errorMessage(p, "expected input");
+			return 0;
+		}
+		while (line[p] == ' ') {
+			if (line.length() > p + 1) {
+				p++;
+			}
+			else {
+				errorMessage(p + 1, "expected input");
+				return 0;
+			}
+		}
+		if (j == 2) {
+			if (line[p] == ',') {
+				p++;
+				while (line[p] == ' ') {
+					if (line.length() > p + 1) {
+						p++;
+					}
+					else {
+						errorMessage(p + 1, "expected input");
+						return 0;
+					}
+				}
+			}
+			else {
+				cout << p;
+				errorMessage(p - 1, "expected ','");
+				return 0;
+			}
+
+		}
+		if (checkNumber(p, j) == 0) {
+			return 0;
+		}
+	}
+	while (line[p] == ' ') {
+		if (line.length() > p + 1) {
+			p++;
+		}
+		else {
+			errorMessage(p + 1, "expected input");
+			return 0;
+		}
+	}
+	if (line[p] != ')') {
+		errorMessage(p + 1, "expected ')'");
+		return 0;
+	}
+	else {
+		if (line.length() == p + 1) {
+			return 1;
+		}
+		else {
+			errorMessage(p + 1, "expected end of line");
+			return 0;
+		}
+	}
+	
 }
 
-void polygon()
-{
+int circle() {
+	if (line[6] != '(') {
+		errorMessage(6, "expected '('");
+		return 0;
+	}
+	if (line.length() == 7) {
+		errorMessage(7, "expected input");
+		return 0;
+	}
+	size_t p = 7;
+	return(parsing(p));
 }
 
-int main()
-{
-    ifstream inFile;
-    inFile.open("geometry-input");
-    ofstream outFile;
-    outFile.open("geometry-output");
-    line = "";
-    if (inFile.is_open() && outFile.is_open()) {
-        while (getline(inFile, line)) {
-            if (line != "") {
-                transform(line.begin(), line.end(), line.begin(), ::tolower);
-                if (line.rfind("circle", 0) == 0
-                    || line.rfind("triangle", 0) == 0
-                    || line.rfind("polygon", 0) == 0) {
-                    if (line.rfind("circle", 0) == 0) {
-                        circle();
-                    }
-                    if (line.rfind("triangle", 0) == 0) {
-                        triangle();
-                    }
-                    if (line.rfind("polygon", 0) == 0) {
-                        polygon();
-                    }
-                    return 0;
-                } else {
-                    outFile << line << endl
-                            << "^" << endl
-                            << "expected circle, triangle or polygon at "
-                               "position 0";
-                    return 0;
-                }
-            }
-        }
-        outFile << "No input detected";
-        return 0;
-    } else if (!inFile.is_open()) {
-        outFile << "Failed to open geometry-input";
-        return 0;
-    } else if (!outFile.is_open()) {
-        outFile << "Failed to open geometry-output";
-        return 0;
-    }
+int main() {
+	ifstream inFile;
+	inFile.open("geometry-input");
+	if (inFile.is_open()) {
+		circleCounter = 0;
+		while (getline(inFile, line)) {
+			transform(line.begin(), line.end(), line.begin(), ::tolower);
+			if (line.rfind("circle", 0) == 0) {
+				if (circle() == 1) {
+					circleCounter++;
+				} 
+				else {
+					return 0;
+				}
+			}
+			else {
+				cout << line << endl << "^" << endl << "expected circle, triangle or polygon at position 0";
+				return 0;
+			}
+		}
+		circleCalculation();
+		print();
+	}
+	else {
+		cout << "Failed to open geometry-input";
+		return 0;
+	}
 }
