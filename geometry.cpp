60bb00fba0cb8a7d22329f49a928ec1144345fb5
Andrew Vaibert <services.weibert@gmail.com>
2023-04-05 15:12:20 +0700

    Modify geometry-input and remove global variables in geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -5,13 +5,10 @@
 #include <cctype>
 #define _USE_MATH_DEFINES
 #include <math.h>
+
 using namespace std;
 
-string line;
-int circleCounter;
-double circleArray[100][100];
-
-int errorMessage(int position, string message) {
+int errorMessage(int position, string message, string line, int circleCounter, double circleArray[][100]) {
 	cout << circleCounter + 1 << ". " << line << endl;
 	int x = 0;
 	for (x = x; x < position; x++) {
@@ -21,7 +18,7 @@ int errorMessage(int position, string message) {
 	return 0;
 }
 
-int print() {
+int print(string line, int circleCounter, double circleArray[][100]) {
 	for (int i = 0; i < circleCounter; i++) {
 		cout << i + 1 << ". " << "circle(" << circleArray[i][0] << ' ' << circleArray[i][1] << ", " << circleArray[i][2] << ')' << endl
 			<< '	' << "perimeter = " << circleArray[i][3] << endl
@@ -36,7 +33,7 @@ int print() {
 	return 0;
 }
 
-int circleCalculation() {
+int circleCalculation(string line, int circleCounter, double circleArray[][100]) {
 	for (int g = 0; g < circleCounter; g++) {
 		circleArray[g][3] = 2 * M_PI * circleArray[g][2];
 		circleArray[g][4] = M_PI * pow(circleArray[g][2], 2);
@@ -47,10 +44,10 @@ int circleCalculation() {
 			if (pow((circleArray[g][2] - circleArray[h][2]), 2) <= pow((circleArray[g][0] - circleArray[h][0]), 2) + pow((circleArray[g][1] - circleArray[h][1]), 2)
 				&& pow((circleArray[g][0] - circleArray[h][0]), 2) + pow((circleArray[g][1] - circleArray[h][1]), 2) <= pow((circleArray[g][2] + circleArray[h][2]), 2)) {
 				int temp = circleArray[g][5];
-				circleArray[g][6 + temp] = h+1;
+				circleArray[g][6 + temp] = h + 1;
 				circleArray[g][5] += 1;
 				temp = circleArray[h][5];
-				circleArray[h][6 + temp] = g+1;
+				circleArray[h][6 + temp] = g + 1;
 				circleArray[h][5] += 1;
 			}
 		}
@@ -58,12 +55,12 @@ int circleCalculation() {
 	return 0;
 }
 
-int checkNumber(size_t &p, int &j) {
+int checkNumber(size_t& p, int& j, string line, int circleCounter, double circleArray[][100]) {
 
 	// If the 1st char is not '+', '-', or digit
 	if (line[p] != '+' && line[p] != '-'
 		&& !(line[p] >= '0' && line[p] <= '9')) {
-		errorMessage(p, "expected Number");
+		errorMessage(p, "expected Number", line, circleCounter, circleArray);
 		return 0;
 	}
 
@@ -94,7 +91,7 @@ int checkNumber(size_t &p, int &j) {
 
 		if (line[p] == ')' && j == 2) {
 			if (line.length() > p + 1) {
-				errorMessage(p + 1, "expected end of line");
+				errorMessage(p + 1, "expected end of line", line, circleCounter, circleArray);
 				return 0;
 			}
 			int end = p;
@@ -108,42 +105,42 @@ int checkNumber(size_t &p, int &j) {
 		// {digit, .}
 		if (line[p] != '.'
 			&& !(line[p] >= '0' && line[p] <= '9')) {
-			errorMessage(p, "expected Number");
+			errorMessage(p, "expected Number", line, circleCounter, circleArray);
 			return 0;
 		}
 		// checks if the char '.' has already occurred before
 		if (line[p] == '.') {
 			if (flagDot == true) {
-				errorMessage(p, "expected Number");
+				errorMessage(p, "expected Number", line, circleCounter, circleArray);
 				return 0;
 			}
 			// If '.' is the last character.
 			if (p + 1 > line.length()) {
-				errorMessage(p, "expected Number");
+				errorMessage(p, "expected Number", line, circleCounter, circleArray);
 				return 0;
 			}
 			// if '.' is not followed by a digit.
 			if (!(line[p + 1] >= '0' && line[p + 1] <= '9')) {
-				errorMessage(p + 1, "expected Number");
+				errorMessage(p + 1, "expected Number", line, circleCounter, circleArray);
 				return 0;
 			}
 			// if there is no digit before '.'.
 			if (!(line[p - 1] >= '0' && line[p - 1] <= '9')) {
-				errorMessage(p, "expected Number");
+				errorMessage(p, "expected Number", line, circleCounter, circleArray);
 				return 0;
 			}
 			// set flagDot = 1 when '.' is encountered.
 			flagDot = true;
 		}
 	}
-	errorMessage(p, "expected input");
+	errorMessage(p, "expected input", line, circleCounter, circleArray);
 	return 0;
 }
 
-int parsing(size_t &p) {
+int parsing(size_t& p, string line, int circleCounter, double circleArray[][100]) {
 	for (int j = 0; j < 3; j++) {
 		if (line.length() == p) {
-			errorMessage(p, "expected input");
+			errorMessage(p, "expected input", line, circleCounter, circleArray);
 			return 0;
 		}
 		while (line[p] == ' ') {
@@ -151,7 +148,7 @@ int parsing(size_t &p) {
 				p++;
 			}
 			else {
-				errorMessage(p + 1, "expected input");
+				errorMessage(p + 1, "expected input", line, circleCounter, circleArray);
 				return 0;
 			}
 		}
@@ -163,19 +160,19 @@ int parsing(size_t &p) {
 						p++;
 					}
 					else {
-						errorMessage(p + 1, "expected input");
+						errorMessage(p + 1, "expected input", line, circleCounter, circleArray);
 						return 0;
 					}
 				}
 			}
 			else {
 				cout << p;
-				errorMessage(p - 1, "expected ','");
+				errorMessage(p - 1, "expected ','", line, circleCounter, circleArray);
 				return 0;
 			}
 
 		}
-		if (checkNumber(p, j) == 0) {
+		if (checkNumber(p, j, line, circleCounter, circleArray) == 0) {
 			return 0;
 		}
 	}
@@ -184,12 +181,12 @@ int parsing(size_t &p) {
 			p++;
 		}
 		else {
-			errorMessage(p + 1, "expected input");
+			errorMessage(p + 1, "expected input", line, circleCounter, circleArray);
 			return 0;
 		}
 	}
 	if (line[p] != ')') {
-		errorMessage(p + 1, "expected ')'");
+		errorMessage(p + 1, "expected ')'", line, circleCounter, circleArray);
 		return 0;
 	}
 	else {
@@ -197,27 +194,30 @@ int parsing(size_t &p) {
 			return 1;
 		}
 		else {
-			errorMessage(p + 1, "expected end of line");
+			errorMessage(p + 1, "expected end of line", line, circleCounter, circleArray);
 			return 0;
 		}
 	}
-	
+
 }
 
-int circle() {
+int circle(string line, int circleCounter, double circleArray[][100]) {
 	if (line[6] != '(') {
-		errorMessage(6, "expected '('");
+		errorMessage(6, "expected '('", line, circleCounter, circleArray);
 		return 0;
 	}
 	if (line.length() == 7) {
-		errorMessage(7, "expected input");
+		errorMessage(7, "expected input", line, circleCounter, circleArray);
 		return 0;
 	}
 	size_t p = 7;
-	return(parsing(p));
+	return(parsing(p, line, circleCounter, circleArray));
 }
 
 int main() {
+	string line;
+	int circleCounter;
+	double circleArray[100][100];
 	ifstream inFile;
 	inFile.open("geometry-input");
 	if (inFile.is_open()) {
@@ -225,9 +225,9 @@ int main() {
 		while (getline(inFile, line)) {
 			transform(line.begin(), line.end(), line.begin(), ::tolower);
 			if (line.rfind("circle", 0) == 0) {
-				if (circle() == 1) {
+				if (circle(line, circleCounter, circleArray) == 1) {
 					circleCounter++;
-				} 
+				}
 				else {
 					return 0;
 				}
@@ -237,8 +237,8 @@ int main() {
 				return 0;
 			}
 		}
-		circleCalculation();
-		print();
+		circleCalculation(line, circleCounter, circleArray);
+		print(line, circleCounter, circleArray);
 	}
 	else {
 		cout << "Failed to open geometry-input";
