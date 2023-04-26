c41e930d53766d9b63bd7a6fcedb6203d7edaec4
Andrew Vaibert <services.weibert@gmail.com>
2023-04-26 17:19:01 +0700

    create dir src and file geometry.h

---
+++ src/libgeometry/geometry.h
@@ -0,0 +1,26 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cctype>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+int errorMessage(int position, string message, string line, int circleCounter);
+
+int print(int circleCounter, double circleArray[][100]);
+
+int circleCalculation(int circleCounter, double circleArray[][100]);
+
+int checkNumber(size_t& p, int& j, string line, int circleCounter, double circleArray[][100]);
+
+int parsing(size_t& p, string line, int circleCounter, double circleArray[][100]);
+
+int circle(string line, int circleCounter, double circleArray[][100]);
+
+#endif
