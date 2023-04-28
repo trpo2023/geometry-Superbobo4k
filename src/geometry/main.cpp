48699d00f685eba02342ba1a82b724dcc8102e73
Andreas Vaibert <services.weibert@gmail.com>
2023-04-28 19:17:48 +0700

    .clang format all src files

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -1,50 +1,43 @@
-#include <libgeometry/geometry.h>
 #include <algorithm>
+#include <cctype>
 #include <fstream>
 #include <iostream>
+#include <libgeometry/geometry.h>
 #include <string>
-#include <cctype>
 #define _USE_MATH_DEFINES
 #include <math.h>
 
 using namespace std;
 
-int main(){
-        string line;
-	int circleCounter;
-	double circleArray[100][100];
-	ifstream inFile;
-	inFile.open("geometry-input");
-	if (inFile.is_open())
-	{
-		circleCounter = 0;
-		while (getline(inFile, line))
-		{
-			transform(line.begin(), line.end(), line.begin(), ::tolower);
-			if (line.rfind("circle", 0) == 0)
-			{
-				if (circle(line, circleCounter, circleArray) == 1)
-				{
-					circleCounter++;
-				}
-				else
-				{
-					return 0;
-				}
-			}
-			else
-			{
-				cout << line << endl << "^" << endl << "expected circle, triangle or polygon at position 0";
-				return 0;
-			}
-		}
-		circleCalculation(circleCounter, circleArray);
-		print(circleCounter, circleArray);
-	}
-	else
-	{
-		cout << "Failed to open geometry-input";
-		return 0;
-	}
-	return 0;
+int main()
+{
+    string line;
+    int circleCounter;
+    double circleArray[100][100];
+    ifstream inFile;
+    inFile.open("geometry-input");
+    if (inFile.is_open()) {
+        circleCounter = 0;
+        while (getline(inFile, line)) {
+            transform(line.begin(), line.end(), line.begin(), ::tolower);
+            if (line.rfind("circle", 0) == 0) {
+                if (circle(line, circleCounter, circleArray) == 1) {
+                    circleCounter++;
+                } else {
+                    return 0;
+                }
+            } else {
+                cout << line << endl
+                     << "^" << endl
+                     << "expected circle, triangle or polygon at position 0";
+                return 0;
+            }
+        }
+        circleCalculation(circleCounter, circleArray);
+        print(circleCounter, circleArray);
+    } else {
+        cout << "Failed to open geometry-input";
+        return 0;
+    }
+    return 0;
 }
